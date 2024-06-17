90508504cebb962a4871da461a200c2202932d54
Юрий Лапин <YRLapin2003@mail.ru>
2024-06-17 16:48:52 +0500

    new login password checks

--- FilmRaterMain/wwwroot/js/login.js
+++ FilmRaterMain/wwwroot/js/login.js
@@ -12,45 +12,105 @@
             regErrorPassword: false,
             regErrorTaken: loginTakenError,
             regErrorFill: false,
+            regErrorLoginTooLong: false,
+            regErrorPasswordTooLong: false,
+            regErrorLoginTooShort: false,
+            regErrorPasswordTooShort: false,
+            regErrorLoginSymbols: false,
+            regErrorPasswordSymbols: false,
         }
     },
     methods: {
-        async tryLogIn() {
+        tryLogIn() {
             vm.logInError = false
             vm.logInErrorFill = false
+
             vm.regErrorPassword = false
             vm.regErrorTaken = false
             vm.regErrorFill = false
+            vm.regErrorLoginTooLong = false
+            vm.regErrorPasswordTooLong = false
+            vm.regErrorLoginTooShort = false
+            vm.regErrorPasswordTooShort = false
+            vm.regErrorLoginSymbols = false
+            vm.regErrorPasswordSymbols = false
+
             if (vm.login == "" || vm.password == "") {
                 vm.logInErrorFill = true
+                return
             }
-            else {
-                $("#input-login-username").val(vm.login)
-                $("#input-login-password").val(vm.password)
-                $("#input-login-camefrom").val(cameFrom)
-                $("#input-login-filmid").val(passedFilmId)
-                $("#try-login-form").submit()
-            }
+
+            $("#input-login-username").val(vm.login)
+            $("#input-login-password").val(vm.password)
+            $("#input-login-camefrom").val(cameFrom)
+            $("#input-login-filmid").val(passedFilmId)
+            $("#try-login-form").submit()
         },
-        async tryRegister() {
+        tryRegister() {
             vm.logInError = false
             vm.logInErrorFill = false
+
             vm.regErrorPassword = false
             vm.regErrorTaken = false
             vm.regErrorFill = false
+            vm.regErrorLoginTooLong = false
+            vm.regErrorPasswordTooLong = false
+            vm.regErrorLoginTooShort = false
+            vm.regErrorPasswordTooShort = false
+            vm.regErrorLoginSymbols = false
+            vm.regErrorPasswordSymbols = false
+
+            if (vm.loginRegister == "" || vm.password1 == "" || vm.password2 == "") {
+                vm.regErrorFill = true
+                return
+            }
+
             if (vm.password1 != vm.password2) {
                 vm.regErrorPassword = true
+                return
             }
-            else if (vm.loginRegister == "" || vm.password1 == "" || vm.password2 == "") {
-                vm.regErrorFill = true
+
+            if (vm.loginRegister.length > 12) {
+                vm.regErrorLoginTooLong = true
+                return
             }
-            else {
-                $("#input-register-username").val(vm.loginRegister)
-                $("#input-register-password").val(vm.password1)
-                $("#input-register-camefrom").val(cameFrom)
-                $("#input-register-filmid").val(passedFilmId)
-                $("#try-register-form").submit()
+
+            if (vm.loginRegister.length < 5) {
+                vm.regErrorLoginTooShort = true
+                return
             }
+
+            if (vm.password1.length > 12) {
+                vm.regErrorPasswordTooLong = true
+                return
+            }
+
+            if (vm.password1.length < 5) {
+                vm.regErrorPasswordTooShort = true
+                return
+            }
+
+            const allowedSymbols = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+
+            for (const c of Array.from(vm.loginRegister))  {
+                if (!allowedSymbols.includes(c)) {
+                    vm.regErrorLoginSymbols = true
+                    return
+                }
+            }
+
+            for (const c of Array.from(vm.password1)) {
+                if (!allowedSymbols.includes(c)) {
+                    vm.regErrorPasswordSymbols = true
+                    return
+                }
+            }
+
+            $("#input-register-username").val(vm.loginRegister)
+            $("#input-register-password").val(vm.password1)
+            $("#input-register-camefrom").val(cameFrom)
+            $("#input-register-filmid").val(passedFilmId)
+            $("#try-register-form").submit()
         }
     }
 }
