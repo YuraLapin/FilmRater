2e2758eb934fa2390aa37c3c579e47ccdb4b54b3
Юрий Лапин <YRLapin2003@mail.ru>
2024-06-24 17:07:56 +0500

    added files

---
+++ FilmRaterMain/Controllers/UtilityClasses/AuthOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FilmRaterMain.Controllers
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "FilmRaterIssuer";
+        public const string AUDIENCE = "FilmRaterAudience";
+        const string KEY = "secret_auth_key_longer_than_32_@!192837";
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+}
---
+++ FilmRaterMain/Controllers/UtilityClasses/DataModels/UserNameAndPassword.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmRaterMain.Controllers
+{
+    public class UserNameAndPassword
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
